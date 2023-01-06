15be188f57706233c847014fe37ce265ea79f02d
shirish-verma <shirishverma@gmail.com>
2023-01-06 17:10:06 +0000

    Corrections.

--- milestone_5.py
+++ milestone_5.py
@@ -1,7 +1,41 @@
-import milestone_4 as hm_cls
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['_' for char in self.word]
+        self.num_letters = len(set([*self.word]))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        self.list_of_guesses.append(guess)
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word.')
+            for char in enumerate(self.word):
+                if char[1] == guess:
+                    self.word_guessed[char[0]] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} lives left.')
+
+    def ask_for_input(self):
+        while True:
+            guess = input('Enter a single letter: ')
+            if guess.isalpha() != True or len(guess) != 1:
+                print('Invalid letter. Please, enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print(f'You already tried that letter!')
+            else:
+                return self.check_guess(guess)
+
 
 def play_game(word_list):
-    game = hm_cls.Hangman(word_list, 5)
+    game = Hangman(word_list, 5)
     while True:
         if game.num_lives == 0:
             print('You lost!')
