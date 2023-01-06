f2e764a26ef0cbc5e897321fc0f88684837bc2ed
shirish-verma <shirishverma@gmail.com>
2023-01-06 14:54:50 +0000

    Adding m4, task 4.

--- milestone_4.py
+++ milestone_4.py
@@ -11,16 +11,17 @@ class Hangman:
 
     def check_guess(self, guess):
         guess = guess.lower()
+        self.list_of_guesses.append(guess)
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
             for char in enumerate(self.word):
                 if char[1] == guess:
                     self.word_guessed[char[0]] = guess
             self.num_letters -= 1
-            return True
         else:
-            print(f'Sorry, {guess} is not in the word. Try again.')
-            return False
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} lives left.')
 
     def ask_for_input(self):
         while True:
@@ -30,13 +31,13 @@ class Hangman:
             elif guess in self.list_of_guesses:
                 print(f'You already tried that letter!')
             else:
-                self.list_of_guesses.append(guess)
                 return self.check_guess(guess)
 
 word_list_1 = ['mango', 'watermelon', 'orange', 'banana', 'grapes']
 
 hm_1 = Hangman(word_list_1)
 print(hm_1.word)
+print(hm_1.list_of_guesses)
 print(hm_1.num_letters, end="\n\n")
 
 hm_1.ask_for_input()
