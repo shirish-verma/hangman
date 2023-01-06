dc6f8430e37f92dffc2e17a33ba90a8caf455895
shirish-verma <shirishverma@gmail.com>
2023-01-06 11:15:16 +0000

    Corrections made.

--- milestone_4.py
+++ milestone_4.py
@@ -6,13 +6,12 @@ class Hangman:
         self.num_lives = num_lives
         self.word = random.choice(self.word_list)
         self.word_guessed = ['_' for char in self.word]
-        self.num_letters = 0
+        self.num_letters = len(set([*self.word]))
         self.list_of_guesses = []
     
-
-
 word_list_1 = ['mango', 'watermelon', 'orange', 'banana', 'grapes']
 
 hm_1 = Hangman(word_list_1)
 
 print(hm_1.word)
+print(hm_1.num_letters)
