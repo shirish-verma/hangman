b99ef06844cce37be046d29b390f2aac0d510912
shirish-verma <shirishverma@gmail.com>
2023-01-06 14:09:24 +0000

    Corrections.

--- milestone_4.py
+++ milestone_4.py
@@ -12,10 +12,10 @@ class Hangman:
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
-            print(f'Good guess! {guess} is in the word "{self.word}"')
+            print(f'Good guess! {guess} is in the word.')
             return True
         else:
-            print(f'Sorry, {guess} is not in the word "{self.word}". Try again.')
+            print(f'Sorry, {guess} is not in the word. Try again.')
             return False
 
     def ask_for_input(self):
@@ -36,6 +36,6 @@ word_list_1 = ['mango', 'watermelon', 'orange', 'banana', 'grapes']
 
 hm_1 = Hangman(word_list_1)
 print(hm_1.word)
-print(hm_1.num_letters, end="\n")
+print(hm_1.num_letters, end="\n\n")
 hm_1.ask_for_input()
 print(hm_1.list_of_guesses)
